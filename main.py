341ca7f3cd207b83ac6b1edf0ddda21cffd0ee55
hezhongq <71187784+hezhongq@users.noreply.github.com>
2021-09-05 18:01:32 -0400

    skeleton updated

--- main.py
+++ main.py
@@ -1,5 +1,79 @@
 """This is a tic-tac-toe game
 """
 
-print("hello world")
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+
+    def move(self, b):
+        # asks user for x-y coordinates
+        # while loop
+
+        # update board
+
+        board.count += 1
+
+
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.count = 0
+        # TODO
+        # initialize board
+
+    def display_board(self):
+        # TODO
+        # display board
+
+
+def check_winner(b, Player):
+    """
+
+    :param board:
+    :param Player:
+    :return: return True if current player win
+    """
+    # check horizontal
+
+
+    # check vertical
+
+
+    # check diagonal
+
+
+
+def check_full(b):
+    if board.count == 9:
+        return True
+    return False
+
+
+if "__main__" == __name__:
+
+    p1 = Player("1")
+    p2 = Player("2")
+    board = Board()
+
+    curr_player = p1
+
+    while not check_winner(board, curr_player) and not check_full(board):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
