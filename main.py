97d3c45a640ac2224da1ea26a20a3e13b59742d2
hezhongq <71187784+hezhongq@users.noreply.github.com>
2021-09-05 18:42:44 -0400

    display_board added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """This is a tic-tac-toe game
 """
 
+BOARD_SIZE = 3
 
 class Player:
     def __init__(self, name):
@@ -11,56 +12,65 @@ class Player:
         # while loop
 
         # update board
-
-        board.count += 1
-
+        b.count += 1
 
 
 class Board:
     def __init__(self):
         self.board = []
         self.count = 0
-        # TODO
         # initialize board
+        for i in range(BOARD_SIZE):
+            temp = []
+            for j in range(BOARD_SIZE):
+                temp.append(" ")
+            self.board.append(temp)
 
     def display_board(self):
-        # TODO
-        # display board
+        s1 = ''
+        for i in range(BOARD_SIZE):
+            s1 += '   ' + str(i + 1)
+        print(s1)
+        for i in range(BOARD_SIZE):
+            temp = str(i + 1)
+            for j in range(BOARD_SIZE):
+                temp += ' ' + str(self.board[i][j]) + ' |'
+            temp = temp[:-1] + ' '
+            print(temp)
+            if i < BOARD_SIZE - 1:
+                temp = ' '
+                for j in range(BOARD_SIZE):
+                    temp += '---+'
+                temp = temp[:-1]
+                print(temp)
 
 
-def check_winner(b, Player):
-    """
+def check_winner(b, player):
+    """ check who is the winner
 
     :param board:
     :param Player:
     :return: return True if current player win
     """
-    # check horizontal
-
-
-    # check vertical
-
-
-    # check diagonal
-
 
 
 def check_full(b):
-    if board.count == 9:
+    if b.count == 9:
         return True
     return False
 
 
-if "__main__" == __name__:
+if '__main__' == __name__:
 
     p1 = Player("1")
     p2 = Player("2")
-    board = Board()
+    b = Board()
 
     curr_player = p1
+    b.display_board()
 
-    while not check_winner(board, curr_player) and not check_full(board):
-        pass
+    # while not check_winner(board, curr_player) and not check_full(board):
+    #     pass
 
 
 
